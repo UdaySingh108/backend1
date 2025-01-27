a830f50013b89e04976eb015b00d66253ed6527c
UdaySingh108 <singh.uday2610@gmail.com>
2025-01-27 09:38:53 +0530

    add folders

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-require('dotenv').config();
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!! How are you?');
+// });
+app.get('/api/jokes',(req,res)=>{
+  const jokes=[
+    {
+      id:1,
+      title:"Joke1",
+      content:"Dk is Beautiful"
+    },
+    {
+      id:2,
+      title:"Joke2",
+      content:"Avnish is innocent"
+    }
+  ];
+  res.send(jokes);
+});
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${process.env.PORT}`)
-})
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
